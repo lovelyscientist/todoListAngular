75f0732a9fce08cd5c9364603804ff2a4a870eb1
Vladyslava Tyshchenko <vtysch@softserveinc.com>
2016-06-03 14:26:26 +0300

    onEnter adding todo and cleaning input implemented

--- client/js/todos/todoController.js
+++ client/js/todos/todoController.js
@@ -4,17 +4,28 @@ angular
 
 ListController.$inject = ['$scope'];
 
-function ListController($scope) {
+function ListController ($scope) {
   $scope.todos = [{text: 'make todo on angular', number: 1}];
   $scope.lastTodo='';
 
-  $scope.addItem = function(item) {
+  $scope.addItem = function (item) {
       $scope.todos.push({text: item, number: $scope.todos.length + 1});
+      $scope.clearInput();
   };
 
-  $scope.onEnter = function(keyEvent) {
-      if (keyEvent.which === 13) {
-          $scope.addItem($scope.lastItem);
-      } 
-  }
+  $scope.onEnter = function (keyEvent) {
+    if (keyEvent.keyCode === 13) {
+        $scope.addItem($scope.lastTodo);
+    }
+  };
+
+  $scope.remove = function (item) {
+      let index = $scope.todos.indexOf(item);
+
+      $scope.todos.splice(index, 1);
+  };
+
+  $scope.clearInput = function () {
+      $scope.lastTodo = '';
+  };
 }
